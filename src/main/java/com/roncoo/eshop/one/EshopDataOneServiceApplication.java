4ddac6c8f8b5d05f8fd9e45b5f50f0987e9dd686
pcy676470 <961676871@qq.com>
2019-02-26 10:10:39 +0800

    111111

--- src/main/java/com/roncoo/eshop/one/EshopDataOneServiceApplication.java
+++ src/main/java/com/roncoo/eshop/one/EshopDataOneServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
@@ -13,7 +12,6 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableCircuitBreaker
-@EnableHystrix
 public class EshopDataOneServiceApplication {
 
 	public static void main(String[] args) {
